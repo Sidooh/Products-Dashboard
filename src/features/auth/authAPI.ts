59c75fb49c9316e6dd1be807bebf1c3caefe921b
Nabcellent <nabcellent.dev@gmail.com>
2022-07-04 07:30:45 +0300

    Modifies Auth to logout user when session token expires
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/features/auth/authAPI.ts
+++ src/features/auth/authAPI.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { CONFIG } from 'config';
-import { JWT } from 'utils/helpers';
 
-const API_URL = `${ CONFIG.sidooh.services.accounts.api.url }/users/signin`;
+const API_URL = `${CONFIG.sidooh.services.accounts.api.url}/users/signin`;
 
 export type LoginRequest = {
     email: string
@@ -13,15 +12,7 @@ export const authAPI = {
     login: async (userData: LoginRequest) => {
         let {data} = await axios.post(API_URL, userData, {withCredentials: true});
 
-        if (data) {
-            data = {
-                token: data.token,
-                user: JWT.decode(data.token),
-                credentials: userData
-            };
-
-            localStorage.setItem('auth', JSON.stringify(data));
-        }
+        if (data) localStorage.setItem('auth', JSON.stringify(data));
 
         return data;
     },

9ce0fc3715832755e482c991d818f7960b13d4cb
Nabcellent <nabcellent.dev@gmail.com>
2022-12-13 18:44:07 +0300

    Fixes ApiResponse export
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/features/accounts/accountsAPI.ts
+++ src/features/accounts/accountsAPI.ts
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { CONFIG } from 'config';
-import { AccountDetails, ApiResponse, ProductAccount } from 'utils/types';
+import { AccountDetails, ProductAccount } from 'utils/types';
 import { RootState } from 'app/store';
+import { ApiResponse } from "@nabcellent/sui-react";
 
 export const accountsAPI = createApi({
     reducerPath: 'accountsApi',

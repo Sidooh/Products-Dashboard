1e670f5989b225c8967c046b63f38af62e848c98
Nabcellent <nabcellent.dev@gmail.com>
2023-01-31 12:57:06 +0300

    Updates sui-react package
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/pages/dashboards/default/transactions/Transactions.tsx
+++ src/pages/dashboards/default/transactions/Transactions.tsx
@@ -2,53 +2,56 @@ import { Card } from 'react-bootstrap';
 import TableActions from 'components/common/TableActions';
 import SidoohAccount from 'components/common/SidoohAccount';
 import { Transaction } from 'utils/types';
-import { DataTable, StatusChip, TableDate } from '@nabcellent/sui-react';
-import moment from 'moment';
+import {
+    currencyFormat,
+    DataTable,
+    getRelativeDateAndTime,
+    PhoneChip,
+    StatusChip,
+    TableDate
+} from '@nabcellent/sui-react';
 
-const Transactions = ({tableTitle, transactions}: { tableTitle: string, transactions: Transaction[] }) => {
+const Transactions = ({ tableTitle, transactions }: { tableTitle: string, transactions: Transaction[] }) => {
     return (
         <Card className={'mb-3'}>
             <Card.Body>
                 <DataTable title={tableTitle} columns={[
                     {
-                        accessorKey: 'customer',
-                        header: 'Customer',
-                        accessorFn: (row: Transaction) => `${row.account?.phone}: ${row.account?.user?.name}`,
-                        cell: ({row}: any) => <SidoohAccount account={row.original.account}/>
+                        accessorKey: 'account',
+                        header: 'Account',
+                        accessorFn: (row: Transaction) => `${row.account.phone}: ${row.account?.user?.name ?? ''}`,
+                        cell: ({ row }: any) => <SidoohAccount account={row.original.account}/>
                     },
                     {
                         accessorKey: 'description',
                         header: 'Description',
-                        cell: ({row}: any) => (
+                        accessorFn: (row: Transaction) => `${row.description}: ${row.destination}`,
+                        cell: ({ row }: any) => (
                             <span>
                                 {row.original.description}<br/>
-                                <small><b>{row.original.destination}</b></small>
+                                <small><b><PhoneChip phone={row.original.destination}/></b></small>
                             </span>
                         )
                     },
                     {
                         accessorKey: 'amount',
                         header: 'Amount',
-                        cell: ({row}: any) => (new Intl.NumberFormat('en-GB', {
-                            style: 'currency',
-                            currency: 'KES'
-                        })).format(row.original.amount)
+                        cell: ({ row }: any) => currencyFormat(row.original.amount)
                     },
                     {
                         accessorKey: 'status',
                         header: 'Status',
-                        cell: ({row}: any) => <StatusChip status={row.original.status}/>
+                        cell: ({ row }: any) => <StatusChip status={row.original.status}/>
                     },
                     {
-                        accessorKey: 'updated_at',
-                        accessorFn: (row: Transaction) => moment(row.created_at).calendar(),
+                        accessorKey: 'created_at',
+                        accessorFn: (row: Transaction) => getRelativeDateAndTime(row.created_at).toString(),
                         header: 'Date',
-                        cell: ({row}: any) => <TableDate date={row.original.updated_at}/>
+                        cell: ({ row }: any) => <TableDate date={row.original.created_at}/>
                     },
                     {
                         id: 'actions',
-                        header: '',
-                        cell: ({row}: any) => <TableActions entityId={row.original.id} entity={'transaction'}/>
+                        cell: ({ row }: any) => <TableActions entityId={row.original.id} entity={'transaction'}/>
                     }
                 ]} data={transactions}/>
             </Card.Body>
--- src/pages/subscriptions/index.tsx
+++ src/pages/subscriptions/index.tsx
@@ -1,12 +1,19 @@
 import { Card } from 'react-bootstrap';
 import { useSubscriptionsQuery } from 'features/subscriptions/subscriptionsAPI';
 import SidoohAccount from 'components/common/SidoohAccount';
-import { DataTable, SectionError, SectionLoader, StatusChip, TableDate } from '@nabcellent/sui-react';
+import {
+    DataTable,
+    getRelativeDateAndTime,
+    SectionError,
+    SectionLoader,
+    StatusChip,
+    TableDate
+} from '@nabcellent/sui-react';
 import { Subscription } from 'utils/types';
 import { logger } from 'utils/logger';
 
 const Subscriptions = () => {
-    let {data: subscriptions, isLoading, isSuccess, isError, error} = useSubscriptionsQuery();
+    let { data: subscriptions, isLoading, isSuccess, isError, error } = useSubscriptionsQuery();
 
     if (isError) return <SectionError error={error}/>;
     if (isLoading || !isSuccess || !subscriptions) return <SectionLoader/>;
@@ -18,10 +25,10 @@ const Subscriptions = () => {
             <Card.Body>
                 <DataTable title={'Subscriptions'} columns={[
                     {
-                        accessorKey: 'customer',
-                        accessorFn: (row: Subscription) => `${row.account?.phone}: ${row.account?.user?.name}`,
-                        header: 'Customer',
-                        cell: ({row}: any) => <SidoohAccount account={row.original.account}/>
+                        accessorKey: 'account',
+                        accessorFn: (row: Subscription) => `${row.account?.phone}: ${row.account?.user?.name ?? ''}`,
+                        header: 'Account',
+                        cell: ({ row }: any) => <SidoohAccount account={row.original.account}/>
                     },
                     {
                         accessorKey: 'type',
@@ -31,22 +38,25 @@ const Subscriptions = () => {
                     {
                         accessorKey: 'status',
                         header: 'Status',
-                        cell: ({row}: any) => <StatusChip status={row.original.status}/>
+                        cell: ({ row }: any) => <StatusChip status={row.original.status}/>
                     },
                     {
                         accessorKey: 'start_date',
+                        accessorFn: (row: Subscription) => getRelativeDateAndTime(row.start_date).toString(),
                         header: 'Start Date',
-                        cell: ({row}: any) => <TableDate date={row.original.start_date} dateOverTime/>,
+                        cell: ({ row }: any) => <TableDate date={row.original.start_date} dateOverTime/>,
                     },
                     {
                         accessorKey: 'end_date',
+                        accessorFn: (row: Subscription) => getRelativeDateAndTime(row.end_date).toString(),
                         header: 'End Date',
-                        cell: ({row}: any) => <TableDate date={row.original.end_date} dateOverTime/>,
+                        cell: ({ row }: any) => <TableDate date={row.original.end_date} dateOverTime/>,
                     },
                     {
                         accessorKey: 'created_at',
+                        accessorFn: (row: Subscription) => getRelativeDateAndTime(row.created_at).toString(),
                         header: 'Created',
-                        cell: ({row}: any) => <TableDate date={row.original.created_at} dateOverTime/>
+                        cell: ({ row }: any) => <TableDate date={row.original.created_at} dateOverTime/>
                     }
                 ]} data={subscriptions}/>
             </Card.Body>
